fd4465b64eda4ab7b26db56ef5fc6e9e4979e207
Marcelo Santos <rocha.mer21@gmail.com>
2023-10-27 21:51:45 -0300

    criado o player

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,22 @@ use macroquad::prelude::*;
 
 #[macroquad::main("breakout")]
 async fn main() {
+    let player_rect = Rect::new(
+        screen_width() * 0.5f32,
+        screen_height() - 100f32,
+        150f32,
+        40f32,
+    );
+
     loop {
+        clear_background(WHITE);
+        draw_rectangle(
+            player_rect.x,
+            player_rect.y,
+            player_rect.w,
+            player_rect.h,
+            DARKBLUE,
+        );
         next_frame().await
     }
 }
