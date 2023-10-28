2efd74f9aca4b286927a7771fad3006a0efbc08b
Marcelo Santos <rocha.mer21@gmail.com>
2023-10-27 22:08:24 -0300

    add movimento do bloco

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,54 @@
 use macroquad::prelude::*;
 
+const PLAYER_SPEED: f32 = 700f32;
+
 struct Player {
-    rect: Rect
+    rect: Rect,
 }
 
 impl Player {
     pub fn new() -> Self {
-        Self { rect: Rect::new(
-            screen_width() * 0.5f32 - 150f32*0.5f32,
-            screen_height() - 100f32,
-            150f32,
-            40f32,
-        ) }
+        Self {
+            rect: Rect::new(
+                screen_width() * 0.5f32 - 150f32 * 0.5f32,
+                screen_height() - 100f32,
+                150f32,
+                40f32,
+            ),
+        }
+    }
+
+    pub fn update(&mut self, dt: f32) {
+        let mut x_move = match (is_key_down(KeyCode::Left), is_key_down(KeyCode::Right)) {
+           (true, false) => -1f32,
+           (false, true) => 1f32,
+           _ => 0f32,
+        };
+
+        self.rect.x += x_move * dt * PLAYER_SPEED;
+
+        if self.rect.x < 0f32 {
+            self.rect.x = 0f32;
+        }
+
+        if self.rect.x > screen_width() - self.rect.w {
+            self.rect.x = screen_width() - self.rect.w;
+        }
     }
 
     pub fn draw(&self) {
-        draw_rectangle(
-            self.rect.x,
-            self.rect.y,
-            self.rect.w,
-            self.rect.h,
-            DARKBLUE,
-        )
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, DARKBLUE)
     }
 }
 
 #[macroquad::main("breakout")]
 async fn main() {
-    let player_rect = Player::new();
+    let mut player = Player::new();
 
     loop {
+        player.update(get_frame_time());
         clear_background(WHITE);
-        player_rect.draw();
+        player.draw();
         next_frame().await
     }
 }
