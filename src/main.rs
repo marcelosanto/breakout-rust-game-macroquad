2e7e7c8ce8c904a0af8fd2f317c3c90b89c7bc2e
Marcelo Santos <rocha.mer21@gmail.com>
2023-10-27 21:57:23 -0300

    criado o struct e impl do Player

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,37 @@
 use macroquad::prelude::*;
 
+struct Player {
+    rect: Rect
+}
+
+impl Player {
+    pub fn new() -> Self {
+        Self { rect: Rect::new(
+            screen_width() * 0.5f32 - 150f32*0.5f32,
+            screen_height() - 100f32,
+            150f32,
+            40f32,
+        ) }
+    }
+
+    pub fn draw(&self) {
+        draw_rectangle(
+            self.rect.x,
+            self.rect.y,
+            self.rect.w,
+            self.rect.h,
+            DARKBLUE,
+        )
+    }
+}
+
 #[macroquad::main("breakout")]
 async fn main() {
-    let player_rect = Rect::new(
-        screen_width() * 0.5f32,
-        screen_height() - 100f32,
-        150f32,
-        40f32,
-    );
+    let player_rect = Player::new();
 
     loop {
         clear_background(WHITE);
-        draw_rectangle(
-            player_rect.x,
-            player_rect.y,
-            player_rect.w,
-            player_rect.h,
-            DARKBLUE,
-        );
+        player_rect.draw();
         next_frame().await
     }
 }
